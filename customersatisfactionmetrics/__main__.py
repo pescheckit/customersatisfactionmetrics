4cf66ad9438193e47f965044807ce5f1d50ae669
Bram Mittendorff <bram@pescheck.nl>
2023-11-24 11:03:50 +0100

    Added docs

--- customersatisfactionmetrics/__main__.py
+++ customersatisfactionmetrics/__main__.py
@@ -1,6 +1,17 @@
+"""Main module for the Customer Satisfaction Metrics application.
+
+This module is the entry point for the application. It contains
+the main function which kicks off the execution of the application.
+"""
+
 def main():
+    """Start the Customer Satisfaction Metrics application.
+
+    This function initializes and starts the main application.
+    It may setup configurations, initialize resources, and
+    perform the primary logic of the application.
+    """
     print("Starting Customer Satisfaction Metrics App...")
-    # Your application's startup code here
 
 
 if __name__ == '__main__':
